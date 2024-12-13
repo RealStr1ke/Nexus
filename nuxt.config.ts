b2478d04b4f14d21ae6023a59166892ee7b6cecf
Thandi Menelas <TMenelas@outlook.com>
2024-12-12 20:36:30 -0500

    Initialized the Nuxt environment (I think)

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+import { defineNuxtConfig } from 'nuxt/config';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	compatibilityDate: '2024-11-01',
+	css: [
+		'~/assets/styles/main.css',
+	],
+	devtools: { enabled: true },
+	modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
+});
